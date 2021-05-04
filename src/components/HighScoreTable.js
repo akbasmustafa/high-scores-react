c17e796b48dbce5ec1ad5d7da74105a35b23822d
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-05-04 04:22:02 +0100

    Done Level 4

--- src/components/HighScoreTable.js
+++ src/components/HighScoreTable.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import allCountryScores from "../scores";
 import PlayerScore from "./PlayerScore";
 import "../App.css";
 
 function HighScoreTable() {
+  const [sortDescending, setSortDescending] = useState(true);
+
   function sortName(a, b) {
     var nameA = a.name.toUpperCase(); // ignore upper and lowercase
     var nameB = b.name.toUpperCase(); // ignore upper and lowercase
@@ -18,17 +20,32 @@ function HighScoreTable() {
   }
 
   function sortNumber(a, b) {
-    return a.s < b.s ? 1 : a.s > b.s ? -1 : 0;
+    return a.s < b.s
+      ? sortDescending
+        ? 1
+        : -1
+      : a.s > b.s
+      ? sortDescending
+        ? -1
+        : 1
+      : 0;
+  }
+  function handleSort() {
+    console.log(sortDescending);
+    setSortDescending(!sortDescending);
   }
 
   return (
     <div className="cardContainer">
+      <div onClick={handleSort} className="customToggle">
+        {sortDescending ? "Descending" : "Ascending"}
+      </div>
       <h1 style={{ color: "blue" }}>High Scores per Countries</h1>
-      {allCountryScores.sort(sortName).map((country) => (
-        <div className="countryCard">
+      {allCountryScores.sort(sortName).map((country, index) => (
+        <div key={index} className="countryCard">
           <h1 style={{ color: "blue" }}>{"HIGH SCORES:" + country.name}</h1>
-          {country.scores.sort(sortNumber).map((score) => (
-            <PlayerScore name={score.n} score={score.s} />
+          {country.scores.sort(sortNumber).map((score, index) => (
+            <PlayerScore key={index} name={score.n} score={score.s} />
           ))}
         </div>
       ))}
